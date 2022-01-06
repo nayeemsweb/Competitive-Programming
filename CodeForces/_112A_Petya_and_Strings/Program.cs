fc4b959c82128a8e65a83d36640e37e033055516
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-01-06 11:21:45 +0600

    _112A_Petya_and_Strings

---
+++ CodeForces/_112A_Petya_and_Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _112A_Petya_and_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstString = Console.ReadLine().ToLower();
+            var secondString = Console.ReadLine().ToLower();
+
+            if (firstString == secondString) Console.WriteLine("0");
+            else
+            {
+                for (var i = 0; i < firstString.Length; i++)
+                {
+                    if (firstString[i] > secondString[i])
+                    {
+                        Console.WriteLine("1");
+                        break;
+                    }
+                    if ((firstString[i] < secondString[i]))
+                    {
+                        Console.WriteLine("-1");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
