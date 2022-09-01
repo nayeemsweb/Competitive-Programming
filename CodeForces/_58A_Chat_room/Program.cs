e7fc9b3ead39fe03692f626799ba302b060cc726
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-09-01 22:34:36 +0600

    Codeforces added

---
+++ CodeForces/_58A_Chat_room/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _58A_Chat_room
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInput = Console.ReadLine().ToLower();
+
+            var targetString = "hello";
+            var targetStringCurrentIndex = 0;
+            var totalLetterAchieved = 0;
+
+            for (var i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] == targetString[targetStringCurrentIndex])
+                {
+                    totalLetterAchieved++;
+                    targetStringCurrentIndex++;
+
+                    if (totalLetterAchieved == 5) break;
+                }
+            }
+
+            if (totalLetterAchieved >= 5) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
---
+++ CodeForces/_617A_Elephant/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _617A_Elephant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var friendsHouseDistance = int.Parse(Console.ReadLine());
+
+            if (friendsHouseDistance % 5 == 0) Console.WriteLine(friendsHouseDistance / 5);
+            else Console.WriteLine((friendsHouseDistance / 5) + 1);
+        }
+    }
+}
