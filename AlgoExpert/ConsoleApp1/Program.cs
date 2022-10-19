2bfde4f97b6972de1e8fa3e079e407d83c226324
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-10-19 21:45:37 +0600

    _231A_Team

---
+++ AlgoExpert/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] items = { 2, 3, 5, 3, 7, 5 };
+            var uniqueItems = items.Distinct();
+
+            foreach (var item in uniqueItems)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ CodeForces/_231A_Team/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _231A_Team
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numOfProblems = int.Parse(Console.ReadLine());
+            var willImplement = 0;
+
+            for (int i = 0; i < numOfProblems; i++)
+            {
+                var ensuredBy = Console.ReadLine().Split(' ');
+                var sum = int.Parse(ensuredBy[0]) + int.Parse(ensuredBy[1]) + int.Parse(ensuredBy[2]);
+
+                if(sum >= 2)
+                {
+                    willImplement++;
+                }
+            }
+
+            Console.WriteLine(willImplement);
+        }
+    }
+}
