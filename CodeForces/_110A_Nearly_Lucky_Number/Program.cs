9ccdcf84401eac254c64c6a0716eedd4b2774ca6
Nayeem Rahman <nayeem12rahman@gmail.com>
2023-02-20 12:21:09 +0600

    110A

---
+++ CodeForces/_110A_Nearly_Lucky_Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _110A_Nearly_Lucky_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input  = Console.ReadLine().ToCharArray();
+
+            if (input.Length <= Math.Pow(10, 18)) 
+            {
+                var count = 0;
+                foreach (char c in input)
+                {
+                    if (c == '-')
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                    
+                    var number = int.Parse(c.ToString());
+
+                    if (number == 4 || number == 7)
+                    {
+                        count++;
+                    }
+                    else
+                        break;
+                }
+
+                if (count == 4 || count == 7)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                    Console.WriteLine("NO");
+            }
+        }
+    }
+}
